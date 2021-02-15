2aa508493c3b0bedb70dab452f451d636fbf322a
boltathi <athithya24@gmail.com>
2021-02-16 00:06:11 +0530

    Nimbella CI/CD Pipline Deployment

--- packages/s/url/index.js
+++ packages/s/url/index.js
@@ -16,7 +16,7 @@ const Url = mongoose.model('urls',{             //Creating URL Model
 }); 
 
 
-async  =>mongoose.connect(DATABASEURI, {                 //Establishing connection with MongoDB
+mongoose.connect(DATABASEURI, {                 //Establishing connection with MongoDB
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }).then(() => console.log("connected to mongo database"))
